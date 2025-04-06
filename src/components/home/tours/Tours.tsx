d85000a2684e3aad0c0e64b9bafcf924adb0b50e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-06 11:36:15 +0300

    added images for mobile

--- src/components/home/tours/Tours.tsx
+++ src/components/home/tours/Tours.tsx
@@ -1,3 +1,27 @@
+import Container from "@/components/shared/container/Container";
+import ToursListMob from "./ToursListMob";
+import ToursListDesk from "./ToursListDesk";
+
 export default function Tours() {
-  return <section>Tours</section>;
+  return (
+    <section className="mb-[148px] xl:mb-[180px]">
+      <Container>
+        <div className="mb-10 md:mb-14">
+          <h2 className="mb-[14px] md:mb-[10px] text-36med xl:text-48med">
+            Наші тури
+          </h2>
+          <p className="max-w-[198px] mb-[23px] md:mb-0 text-14med xl:text-16med">
+            Мрієте про незабутню подорож?
+          </p>
+          <p>
+            Ми створили тури для кожного, хто прагне відкрити світ: від
+            романтичних прогулянок до захопливих пригод. Обирайте напрямок та
+            вирушайте в подорож мрії!
+          </p>
+        </div>
+        <ToursListMob />
+        <ToursListDesk />
+      </Container>
+    </section>
+  );
 }
---
+++ src/components/home/tours/ToursListDesk.tsx
@@ -0,0 +1,3 @@
+export default function ToursListDesk() {
+  return <div>ToursListDesk</div>;
+}
