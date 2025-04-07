0e58e4cfa1709a8d39961cf5bc91272dd38af061
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-07 19:07:57 +0300

    added arrow

--- src/components/home/smartTours/SmartTours.tsx
+++ src/components/home/smartTours/SmartTours.tsx
@@ -8,7 +8,10 @@ export default function SmartTours() {
   return (
     <section id={SECTION_ID} className="relative mb-[148px] xl:mb-[180px]">
       <Container className="md:flex mb-10 md:mb-0">
-        <div className="relative w-full md:w-[325px] xl:w-[455px] h-[408px] md:h-[332px] xl:h-[408px] py-9 md:mr-5 rounded-[12px] text-white overflow-hidden">
+        <div
+          className="relative w-full md:w-[325px] xl:w-[455px] h-[408px] md:h-[332px] xl:h-[408px] py-9 md:mr-5 rounded-[12px] 
+        text-white overflow-hidden"
+        >
           <Image
             src="/images/home/smart/smartBg.webp"
             alt="background"
@@ -16,6 +19,7 @@ export default function SmartTours() {
             sizes="(max-width: 768px) 100vw, 33vw"
             className="absolute top-0 left-0 -z-20 object-cover"
           />
+
           <div className="absolute -z-10 top-[-164px] left-[calc(50%-163px)] w-[326px] h-[341px] rounded-full bg-main"></div>
           <h2 className="max-w-[143px] xl:max-w-[257px] mx-auto mb-[147px] md:mb-[73px] xl:mb-[119px] text-36med xl:text-48med text-center">
             SMART тури
@@ -28,9 +32,15 @@ export default function SmartTours() {
         <div className="hidden md:block md:w-[calc(100%-345px)] xl:w-[calc(100%-475px)]">
           <SmartToursSwiper />
         </div>
+        <Image
+          src="/images/home/smart/arrow.svg"
+          alt="arrow"
+          width="299"
+          height="98"
+          className="hidden md:block absolute -z-30 bottom-[-38px] left-[calc(50%-266px)] lg:left-[calc(50%-316px)] xl:left-[calc(50%-352px)] object-cover"
+        />
       </Container>
       <div className="md:hidden">
-        {" "}
         <SmartToursSwiper />
       </div>
     </section>
