20f51de792115d75aabb14062a6c5b392dd79f62
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-07 18:51:09 +0300

    added smart section for desktop

--- src/components/home/smartTours/SmartTours.tsx
+++ src/components/home/smartTours/SmartTours.tsx
@@ -6,9 +6,9 @@ const SECTION_ID = "smart-tours";
 
 export default function SmartTours() {
   return (
-    <section id={SECTION_ID} className="mb-[148px] xl:mb-[180px]">
-      <Container className="mb-10">
-        <div className="relative w-full h-[408px] py-9 rounded-[12px] text-white overflow-hidden">
+    <section id={SECTION_ID} className="relative mb-[148px] xl:mb-[180px]">
+      <Container className="md:flex mb-10 md:mb-0">
+        <div className="relative w-full md:w-[325px] xl:w-[455px] h-[408px] md:h-[332px] xl:h-[408px] py-9 md:mr-5 rounded-[12px] text-white overflow-hidden">
           <Image
             src="/images/home/smart/smartBg.webp"
             alt="background"
@@ -17,7 +17,7 @@ export default function SmartTours() {
             className="absolute top-0 left-0 -z-20 object-cover"
           />
           <div className="absolute -z-10 top-[-164px] left-[calc(50%-163px)] w-[326px] h-[341px] rounded-full bg-main"></div>
-          <h2 className="max-w-[143px] xl:max-w-[257px] mx-auto mb-[147px] xl:mb-[119px] text-36med xl:text-48med text-center">
+          <h2 className="max-w-[143px] xl:max-w-[257px] mx-auto mb-[147px] md:mb-[73px] xl:mb-[119px] text-36med xl:text-48med text-center">
             SMART тури
           </h2>
           <p className="max-w-[221px] xl:max-w-[342px] mx-auto text-16reg xl:text-20reg text-center">
@@ -25,8 +25,14 @@ export default function SmartTours() {
             індивідуальний сервіс та вигідні умови.
           </p>
         </div>
+        <div className="hidden md:block md:w-[calc(100%-345px)] xl:w-[calc(100%-475px)]">
+          <SmartToursSwiper />
+        </div>
       </Container>
-      <SmartToursSwiper />
+      <div className="md:hidden">
+        {" "}
+        <SmartToursSwiper />
+      </div>
     </section>
   );
 }
--- src/components/shared/tourCard/TourCard.tsx
+++ src/components/shared/tourCard/TourCard.tsx
@@ -13,7 +13,7 @@ export default function TourCard({ tour }: TourCardProps) {
   return (
     <Link
       href={`/tours/${slug}`}
-      className="block relative max-w-[210px] md:max-w-[226px] lg:max-w-[228px] xl:max-w-[265px] rounded-[12px] overflow-hidden shadow-card"
+      className="block relative max-w-[210px] md:max-w-[226px] lg:max-w-[228px] xl:max-w-[265px] min-h-full rounded-[12px] overflow-hidden shadow-card"
     >
       <div className="absolute -z-10 top-0 left-0 w-full aspect-[210/129] xl:aspect-[265/170]">
         <Image
@@ -23,11 +23,13 @@ export default function TourCard({ tour }: TourCardProps) {
           sizes="(max-width: 768px) 100vw, 33vw"
         />
       </div>
-      <div className="px-4 py-7 mt-[105px] xl:mt-[139px] rounded-[12px] bg-white">
+      <div className="px-4 py-7 xl:py-[30px] mt-[105px] xl:mt-[139px] rounded-[12px] bg-white">
         <h3 className="mb-[19px] text-16semi xl:text-20semi line-clamp-1">
           {title}
         </h3>
-        <p className="mb-[14px] xl:mb-[22px] text-12light">{description}</p>
+        <p className="min-h-[74px] mb-[14px] xl:mb-[22px] text-12light line-clamp-5">
+          {description}
+        </p>
         <p className="mb-[14px] xl:mb-[22px] text-12semi xl:text-14semi">
           {duration}&nbsp;днів
         </p>
