dcf7a9ef25124a33a72f6bb879fb37ca7878feee
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-09 18:21:53 +0300

    added offers list for mobile

--- src/components/corporate/offers/Offers.tsx
+++ src/components/corporate/offers/Offers.tsx
@@ -1,9 +1,14 @@
+import Container from "@/components/shared/container/Container";
+import OffersListMob from "./OffersListMob";
 import OffersTitle from "./OffersTitle";
 
 export default function Offers() {
   return (
     <section className="mb-[148px] xl:mb-[180px]">
       <OffersTitle />
+      <Container>
+        <OffersListMob />
+      </Container>
     </section>
   );
 }
