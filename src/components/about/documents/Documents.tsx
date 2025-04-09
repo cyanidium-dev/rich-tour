136cdf02d46cba57fe990d2a8d137b8bf09afb08
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-09 14:32:09 +0300

    added documents button

--- src/components/about/documents/Documents.tsx
+++ src/components/about/documents/Documents.tsx
@@ -1,9 +1,26 @@
+import { allDocumentsUrl } from "./mockedData";
+import Container from "@/components/shared/container/Container";
 import DocumentsTitle from "./DocumentsTitle";
+import MainButton from "@/components/shared/buttons/MainButton";
 
 export default function Documents() {
   return (
-    <section className="pt-[65px] xl:pt-12 pb-[55px] xl:pb-[73px] mb-[148px] xl:mb-[180px] bg-black">
-      <DocumentsTitle />
+    <section className="mb-[148px] xl:mb-[180px]">
+      <div className="pt-[65px] xl:pt-12 pb-[55px] xl:pb-[73px] mb-10 xl:mb-6 bg-black">
+        <DocumentsTitle />
+      </div>
+      <Container>
+        <a
+          href={allDocumentsUrl || ""}
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+          className="block w-full max-w-[346px] mx-auto"
+        >
+          <MainButton variant="black" className="w-full h-[62px] text-16med">
+            Завантажити документи
+          </MainButton>
+        </a>
+      </Container>
     </section>
   );
 }
