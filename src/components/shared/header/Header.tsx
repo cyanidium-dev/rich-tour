68e3725220574aa0bf1a5aad9b36591a2470e9ea
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-10 15:50:32 +0300

    added animation to the footer

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import HeaderMob from "./HeaderMob";
 import HeaderDesk from "./HeaderDesk";
+import AnimatedWrapper from "../animation/AnimatedWrapper";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-dvw bg-white">
+    <AnimatedWrapper
+      key={pathname}
+      as="header"
+      className="fixed top-0 left-0 z-50 w-dvw bg-white"
+    >
       <HeaderMob />
       <HeaderDesk />
-    </header>
+    </AnimatedWrapper>
   );
 }
