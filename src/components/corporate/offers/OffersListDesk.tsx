240c1d388a265054c0a30e223d6357d760e41699
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-11 15:32:16 +0300

    added offers animations

--- src/components/corporate/offers/OffersListDesk.tsx
+++ src/components/corporate/offers/OffersListDesk.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import { fadeInAnimation } from "@/components/shared/animation/animationVariants";
+import AnimatedWrapper from "@/components/shared/animation/AnimatedWrapper";
 
 export default function OffersListDesk() {
   return (
     <div className="hidden md:flex gap-5">
       {/* Ліва колонка: велике зображення з круглим червоним блоком */}
-      <div className="relative flex items-end w-[calc(33.3%-10px)] aspect-[360/484] p-5 lg:p-8 rounded-[12px] overflow-hidden">
+      <AnimatedWrapper
+        animation={fadeInAnimation({ x: -30, delay: 0.8 })}
+        className="relative flex items-end w-[calc(33%-10px)] aspect-[360/484] p-5 lg:p-8 rounded-[12px] overflow-hidden"
+      >
         <Image
           src="/images/corporate/offers/imageOneDesk.webp"
           alt="background"
@@ -15,11 +20,14 @@ export default function OffersListDesk() {
           Корпоративні тури та тімбілдинги
         </p>
         <div className="absolute -z-10 bottom-[-128px] left-[-38px] w-[270px] lg:w-[320px] aspect-[1/1] bg-main rounded-full"></div>
-      </div>
+      </AnimatedWrapper>
 
-      <div className="flex flex-col justify-between w-[calc(66.7%-10px)]">
+      <div className="flex flex-col justify-between w-[calc(67%-10px)]">
         {/* Правий блок */}
-        <div className="relative flex items-end aspect-[740/194] p-5 lg:p-8 rounded-[12px] overflow-hidden">
+        <AnimatedWrapper
+          animation={fadeInAnimation({ x: 30, delay: 1.2 })}
+          className="relative flex items-end aspect-[740/194] p-5 lg:p-8 rounded-[12px] overflow-hidden"
+        >
           <Image
             src="/images/corporate/offers/imageTwoDesk.webp"
             alt="background"
@@ -29,9 +37,12 @@ export default function OffersListDesk() {
           <p className="max-w-[332px] text-20med xl:text-24med text-white">
             Участь у виставках та конференціях
           </p>
-        </div>
+        </AnimatedWrapper>
 
-        <div className="flex gap-x-5">
+        <AnimatedWrapper
+          animation={fadeInAnimation({ y: 30, delay: 1.6 })}
+          className="flex gap-x-5"
+        >
           {/* Нижній середній блок */}
           <div className="relative flex items-end aspect-[360/264] w-[calc(50%-10px)] p-5 lg:p-8 rounded-[12px] overflow-hidden">
             <Image
@@ -58,7 +69,7 @@ export default function OffersListDesk() {
               Організація ділових подорожей
             </p>
           </div>
-        </div>
+        </AnimatedWrapper>
       </div>
     </div>
   );
