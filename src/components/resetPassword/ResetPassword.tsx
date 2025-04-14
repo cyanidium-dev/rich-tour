1d3430299ca70c12a98b92695a32189af0f5fb9c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-14 17:14:21 +0300

    added reset password animations

--- src/components/resetPassword/ResetPassword.tsx
+++ src/components/resetPassword/ResetPassword.tsx
@@ -1,3 +1,5 @@
+import { fadeInAnimation } from "@/components/shared/animation/animationVariants";
+import AnimatedWrapper from "@/components/shared/animation/AnimatedWrapper";
 import Container from "../shared/container/Container";
 import ResetPasswordFormWithNotifications from "./ResetPasswordFormWithNotifications";
 
@@ -5,13 +7,23 @@ export default function ResetPassword() {
   return (
     <section className="mt-[18px] mb-[148px] xl:mb-[180px]">
       <Container>
-        <div className="max-w-[740px] pt-[120px] pb-[178px] xl:py-[74px] sm:px-8 mx-auto sm:border border-black rounded-[16px]">
-          <h1 className="mb-6 text-24semi text-center">Відновлення паролю</h1>
-          <p className="max-w-[197px] mb-6 mx-auto text-14reg text-center">
+        <AnimatedWrapper className="max-w-[740px] pt-[120px] pb-[178px] xl:py-[74px] sm:px-8 mx-auto sm:border border-black rounded-[16px]">
+          <AnimatedWrapper
+            as="h1"
+            animation={fadeInAnimation({ y: 30 })}
+            className="mb-6 text-24semi text-center"
+          >
+            Відновлення паролю
+          </AnimatedWrapper>
+          <AnimatedWrapper
+            as="p"
+            animation={fadeInAnimation({ y: 30, delay: 0.4 })}
+            className="max-w-[197px] mb-6 mx-auto text-14reg text-center"
+          >
             Вкажіть новий пароль для свого кабінету.
-          </p>
+          </AnimatedWrapper>
           <ResetPasswordFormWithNotifications />
-        </div>
+        </AnimatedWrapper>
       </Container>
     </section>
   );
