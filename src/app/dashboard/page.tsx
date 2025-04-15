fccbeb3d0ed1cfcb8b628cce84d6cde9cdf61f9a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-15 19:58:49 +0300

    added agent contract section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import AgentContract from "@/components/dashboard/agentContract/AgentContract";
 import Hero from "@/components/dashboard/hero/Hero";
 
 export default function DashboardPage() {
   return (
     <>
       <Hero />
+      <AgentContract />
     </>
   );
 }
