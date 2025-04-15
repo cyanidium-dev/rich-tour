a0d331c7b5bbc6793a81d5d9946fd3724be3ed77
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-15 13:19:32 +0300

    wrapped tours in suspense

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react";
 import Tours from "@/components/tours/Tours";
+import Loader from "@/components/shared/loader/Loader";
 
 export default function ToursPage() {
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <Tours />
-    </>
+    </Suspense>
   );
 }
