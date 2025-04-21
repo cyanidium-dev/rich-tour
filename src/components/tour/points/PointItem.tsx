09c691853a31f55887555f98896734931e9775a2
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-21 20:17:09 +0300

    added points animation

--- src/components/tour/points/PointItem.tsx
+++ src/components/tour/points/PointItem.tsx
@@ -1,15 +1,21 @@
+import { fadeInAnimation } from "@/components/shared/animation/animationVariants";
+import AnimatedWrapper from "@/components/shared/animation/AnimatedWrapper";
+import AnimatedListItem from "@/components/shared/animation/AnimatedListItem";
+
 interface PointItemProps {
   point: string;
 }
 
 export default function PointItem({ point }: PointItemProps) {
   return (
-    <li className="relative w-fit">
+    <AnimatedListItem className="relative w-fit">
       <p className=" py-2 px-8 rounded-full border border-black bg-white text-18reg">
         {point}
       </p>
-
-      <div className="absolute bottom-[-30px] xl:bottom-[-34px] left-[calc(50%-8px)] size-4 rounded-full bg-main"></div>
-    </li>
+      <AnimatedWrapper
+        animation={fadeInAnimation({ scale: 0.9 })}
+        className="absolute bottom-[-30px] xl:bottom-[-34px] left-[calc(50%-8px)] size-4 rounded-full bg-main"
+      ></AnimatedWrapper>
+    </AnimatedListItem>
   );
 }
--- src/components/tour/points/Points.tsx
+++ src/components/tour/points/Points.tsx
@@ -1,4 +1,9 @@
 import { Tour } from "@/types/tour";
+import {
+  fadeInAnimation,
+  listVariants,
+} from "@/components/shared/animation/animationVariants";
+import AnimatedWrapper from "@/components/shared/animation/AnimatedWrapper";
 import Container from "@/components/shared/container/Container";
 import PointItem from "./PointItem";
 import Image from "next/image";
@@ -17,22 +22,35 @@ export default function Points({ tour }: PointsProps) {
     <section className="mb-[74px] xl:mb-[145px]">
       <Container>
         <div className="relative flex flex-col xl:flex-row gap-y-7 xl:gap-x-[46px] items-center xl:items-start pb-[60px] xl:pb-0 xl:pt-2">
-          <div className="absolute top-0 xl:top-[calc(50%+16px)] left-[calc(50%-1px)] xl:left-[4px] -z-10 w-[2px] xl:w-[calc(100%-4px)] h-full xl:h-[2px] bg-gradient-to-b xl:bg-gradient-to-r from-main to-black">
+          <AnimatedWrapper
+            animation={fadeInAnimation({ delay: 0.8 })}
+            className="absolute top-0 xl:top-[calc(50%+16px)] left-[calc(50%-1px)] xl:left-[4px] -z-10 w-[2px] xl:w-[calc(100%-4px)] h-full xl:h-[2px] bg-gradient-to-b xl:bg-gradient-to-r from-main to-black"
+          >
             <ArrowHeadIcon className="absolute bottom-[-6.5px] xl:bottom-[-6px] left-[-6px] xl:left-auto xl:right-[-5px] -rotate-90 xl:rotate-180 h-4 text-black" />
-          </div>
-          <div className="xl:mt-2">
+          </AnimatedWrapper>
+          <AnimatedWrapper
+            animation={fadeInAnimation({ x: -30 })}
+            className="xl:mt-2"
+          >
             <Image
               src="/images/tour/points/bus.svg"
               alt="bus"
               width="290"
               height="99"
             />
-          </div>
-          <ul className="flex flex-col xl:flex-row items-center gap-y-[90px] xl:gap-x-[49px]">
+          </AnimatedWrapper>
+          <AnimatedWrapper
+            as="ul"
+            animation={listVariants({
+              staggerChildren: 0.5,
+              delayChildren: 0.4,
+            })}
+            className="flex flex-col xl:flex-row items-center gap-y-[90px] xl:gap-x-[49px]"
+          >
             {points.map((point, idx) => (
               <PointItem key={idx} point={point} />
             ))}
-          </ul>
+          </AnimatedWrapper>
         </div>
       </Container>
     </section>
