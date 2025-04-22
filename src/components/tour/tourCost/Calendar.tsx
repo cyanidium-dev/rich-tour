7b1567d015a2d68e08e43d31c66f59656e987389
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-23 00:16:26 +0300

    updated styles

--- src/components/tour/tourCost/Calendar.tsx
+++ src/components/tour/tourCost/Calendar.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
 import { DayPicker, Locale, getDefaultClassNames } from "react-day-picker";
 import { uk } from "react-day-picker/locale";
-import { DayButtonProps } from "react-day-picker";
-import { tourDepartures } from "./mockedData";
 import "react-day-picker/style.css";
 import Legend from "./Legend";
 import MainButton from "@/components/shared/buttons/MainButton";
+import DayButton from "./DayButton";
 
 export default function Calendar() {
   const [selected, setSelected] = useState<Date>();
@@ -35,87 +33,6 @@ export default function Calendar() {
   const initialMonth = today;
   const maxDate = new Date(nextYear, 11, 31);
 
-  function getDayData(date: Date) {
-    const year = date.getFullYear();
-    const month = `${year}-${String(date.getMonth() + 1).padStart(2, "0")}`;
-    const day = date.getDate();
-
-    const monthData = tourDepartures[month];
-    if (!monthData) return null;
-
-    return monthData.find((item) => item.day === day);
-  }
-
-  function DayButton(props: DayButtonProps) {
-    const { day, modifiers, ...buttonProps } = props;
-    const date = day.date;
-    const dayData = getDayData(date);
-    const status = dayData?.status || [];
-
-    const iconStatuses = ["guaranteed", "hot", "discount"];
-    const borderStatuses = {
-      available: "#73A91C",
-      fewSeats: "#FFB429",
-      onRequest: "#4C6FFF",
-      noSeats: "#E43A3A",
-      noDeparture: "#18181B",
-    };
-
-    const showIcons = status.filter((s) => iconStatuses.includes(s));
-    const borderStatus = status.find((s) => !iconStatuses.includes(s));
-
-    const borderColor = borderStatus
-      ? borderStatuses[borderStatus]
-      : "transparent";
-    const textColor = borderStatus ? borderStatuses[borderStatus] : "black";
-
-    const statusIcons: Record<string, string> = {
-      guaranteed: "/images/icons/calendar/guaranteed.svg",
-      hot: "/images/icons/calendar/hot.svg",
-      discount: "/images/icons/calendar/discount.svg",
-    };
-
-    const hasPrice = !!dayData?.price;
-
-    return (
-      <button
-        {...buttonProps}
-        className={`w-[44px] h-[49px] rounded-[6px] border-2 relative px-[2px] ${
-          hasPrice
-            ? "flex flex-col items-center justify-start pt-[2px]"
-            : "flex items-center justify-center"
-        }`}
-        style={{
-          borderColor: borderColor,
-          color: textColor,
-        }}
-      >
-        <span className={`${hasPrice ? "text-12med" : "text-16reg"}`}>
-          {date.getDate()}
-        </span>
-
-        {hasPrice && (
-          <span className="text-10semi text-black mt-[1px]">
-            {dayData.price}$
-          </span>
-        )}
-
-        {showIcons.length > 0 && (
-          <div className="absolute bottom-[2px] left-1 flex gap-[6px]">
-            {showIcons.map((s) => {
-              const icon = statusIcons[s];
-              if (!icon) return null;
-
-              return (
-                <Image key={s} src={icon} alt={s} width={13} height={13} />
-              );
-            })}
-          </div>
-        )}
-      </button>
-    );
-  }
-
   return (
     <div className="md:w-[calc(33.3%-26.67px)] flex flex-col gap-y-7">
       <div className="rounded-[28px] border border-black">
@@ -141,7 +58,6 @@ export default function Calendar() {
             caption_label: "hidden",
             dropdown: `bg-white appearance-none bg-[url('/images/icons/calendar/arrow.svg')] bg-no-repeat bg-[right_8px_center] bg-[length:16px_16px] pr-6 outline-none text-16reg cursor-pointer`,
             day: "text-16reg",
-            // day_button: `w-[44px] h-[49px] border border-main rounded-[6px]`,
             weekday: "mt-[10px] py-3 text-16reg",
             week: "h-[57.5px]",
             chevron: "size-[18px]",
