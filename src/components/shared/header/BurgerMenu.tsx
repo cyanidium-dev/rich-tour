dccc47a9d44dcfc3b6b3d1740649fa77f5e64313
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-06 12:33:17 +0300

    updated burger menu animation ease function

--- src/components/shared/header/BurgerMenu.tsx
+++ src/components/shared/header/BurgerMenu.tsx
@@ -6,11 +6,15 @@ import AnimatedArrow from "./AnimatedArrow";
 
 const burgerMenuVariants = {
   hidden: { x: 0, opacity: 0 },
-  visible: { x: 0, opacity: 1, transition: { duration: 0.3, ease: "easeOut" } },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.3, ease: [0.25, 0.1, 0.25, 1] as const },
+  },
   exit: {
     x: 0,
     opacity: 0,
-    transition: { duration: 0.3, ease: "easeIn" },
+    transition: { duration: 0.3, ease: [0.42, 0, 1, 1] as const },
   },
 };
 
