852e093ed2baa158f0ae4debd30db01449104673
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2026-03-23 13:03:40 +0100

    fix

--- src/app/agent/page.tsx
+++ src/app/agent/page.tsx
@@ -3,16 +3,30 @@ import AgentContract from "@/components/agent/agentContract/AgentContract";
 import Hero from "@/components/agent/hero/Hero";
 import Menu from "@/components/agent/menu/Menu";
 
+const getExistProperties = user => {
+    const result: string[] = [];
+    if(!user.legalCompanyName) {
+        result.push("Юридичну назву фірми");
+    }
+    if(!user.edrpou) {
+        result.push("ЄДРПОУ");
+    }
+    if(!user.taxForm) {
+        result.push("Форму оподактування");
+    }
+    return result;
+}
+
 export default async function DashboardPage() {
     const user = await getAuthAgentProfile();
 
     if (!user) {
         return null;
     }
-
+    const existProperties = getExistProperties(user);
     return (
         <>
-            <Hero />
+            <Hero existProperties={existProperties} />
             <AgentContract />
             <Menu user={user} />
         </>
--- src/components/agent/hero/Hero.tsx
+++ src/components/agent/hero/Hero.tsx
@@ -1,13 +1,31 @@
+"use client";
+
+import {useState} from "react";
 
 import { fadeInAnimation } from "@/components/shared/animation/animationVariants";
 import AnimatedWrapper from "@/components/shared/animation/AnimatedWrapper";
 import Container from "@/components/shared/container/Container";
 
+import NotificationPopUp from "@/components/shared/pop-ups/NotificationPopUp";
+import Backdrop from "@/components/shared/backdrop/Backdrop";
+
 import AnimatedScribbleArrow from "@/components/agent/menu/content/AnimatedScribbleArrow";
 
-export default function Hero() {
+export default function Hero({existProperties}: {existProperties: string[]}) {
+    const [isNotificationShown, setIsNotificationShown] = useState(Boolean(existProperties.length));
+
     return (
         <section className="relative">
+            <NotificationPopUp
+                title={"Не вся інформація заповнена"}
+                description={`Заповніть будь ласка цю інформацію в налаштуваннях: ${existProperties.join(", ")}`}
+                isPopUpShown={isNotificationShown}
+                setIsPopUpShown={setIsNotificationShown}
+            />
+            <Backdrop
+                isVisible={isNotificationShown}
+                onClick={() => setIsNotificationShown(false)}
+            />
             <Container className="relative pt-[40px] xl:pt-[40px] pb-[100px] xl:pb-[86px]">
                 <div className="relative grid grid-cols-1 lg:grid-cols-[1fr_480px] items-center gap-y-[32px] lg:gap-x-10">
                     <AnimatedWrapper
